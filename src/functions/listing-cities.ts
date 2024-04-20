d94ecdccfadb63c644958cf112406cec6e03b0e6
casulit <chrisgelosulit@gmail.com>
2024-04-20 20:56:06 +0800

    feat(listing-cities): Add city listing endpoint

---
+++ src/functions/listing-cities.ts
@@ -0,0 +1,77 @@
+import {
+    app,
+    HttpRequest,
+    HttpResponseInit,
+    InvocationContext,
+} from '@azure/functions'
+import { getPoolDb } from '../database/neon'
+import { redis } from '../database/redis'
+
+export async function listingCities(
+    _request: HttpRequest,
+    _context: InvocationContext
+): Promise<HttpResponseInit> {
+    const databaseUrl = process.env['NEON_DATABASE_URL']
+
+    if (!databaseUrl) {
+        return {
+            jsonBody: {
+                message: 'Database URL not defined.',
+            },
+            status: 500,
+        }
+    }
+
+    const { client, pool } = await getPoolDb(databaseUrl)
+
+    try {
+        const redisCredentials = {
+            cacheHostName: process.env['AZURE_REDIS_URL'],
+            cachePassword: process.env['AZURE_REDIS_PASSOWRD'],
+        }
+
+        const rd = await redis(redisCredentials)
+
+        const cities = await rd.get('cities')
+
+        if (cities) {
+            return {
+                jsonBody: {
+                    data: JSON.parse(cities),
+                },
+            }
+        }
+
+        const query = await client.query(
+            `SELECT 
+              c.name,
+              r.name AS region
+            FROM city AS c
+            INNER JOIN region AS r ON r.id = c.region_id 
+            ORDER BY c.name ASC`
+        )
+
+        await rd.set('cities', JSON.stringify(query.rows))
+
+        return {
+            jsonBody: {
+                data: query.rows,
+            },
+        }
+    } catch (error) {
+        return {
+            jsonBody: {
+                message: 'Something went wrong.' || error?.message,
+            },
+            status: 500,
+        }
+    } finally {
+        pool.end()
+    }
+}
+
+app.http('listing-cities', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: listingCities,
+})
